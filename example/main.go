e968c64bbacd4f0a889d57076919d22a24e80089
TeNg <tenm@urbox.vn>
2022-02-14 01:18:03 +0700

    update example

--- example/main.go
+++ example/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 	"log"
+	"strconv"
+	"strings"
 
 	"github.com/teng231/smartcache"
 )
@@ -14,17 +16,28 @@ type D struct {
 }
 
 func getDataFromRedis(cond interface{}) (interface{}, error) {
-	condI := cond.(int)
-	if condI%2 == 0 {
-		return D{B: condI, A: "redis hit"}, nil
+	condI := cond.(string)
+	items := strings.Split(condI, ".")
+	id := items[len(items)-1]
+	idInt, _ := strconv.ParseInt(id, 10, 64)
+	if idInt%2 == 0 {
+		return D{B: int(idInt), A: "redis hit"}, nil
 	}
 	return nil, errors.New("not found")
 }
 
+func setDataRedis(key, val interface{}) error {
+	log.Print(key, val)
+	return nil
+}
 func getDataFromMysql(cond interface{}) (interface{}, error) {
-	condI := cond.(int)
-	if condI%3 == 0 {
-		return D{B: condI, A: "db hit"}, nil
+	condI := cond.(string)
+	items := strings.Split(condI, ".")
+	id := items[len(items)-1]
+	idInt, _ := strconv.ParseInt(id, 10, 64)
+	log.Print(strconv.ParseInt(id, 16, 64))
+	if idInt%3 == 0 {
+		return D{B: int(idInt), A: "db hit"}, nil
 	}
 	return nil, errors.New("not found")
 }
@@ -48,6 +61,7 @@ func main() {
 	if hit {
 		log.Print("nothing hit")
 	}
+	// simple reader
 	out2 := &D{}
 	cache.Select(context.TODO(), "key1").Get(2, nil, getDataFromRedis, getDataFromMysql).Exec(out2)
 	log.Print(2, out2)
@@ -55,4 +69,15 @@ func main() {
 	log.Print(3, out2)
 	cache.Select(context.TODO(), "key1").Get(6, nil, getDataFromRedis, getDataFromMysql).Exec(out2)
 	log.Print(6, out2)
+
+	cache.Select(context.TODO(), "key1").Get(6, nil, getDataFromRedis, getDataFromMysql).Exec(out2)
+	log.Print(6, out2)
+
+	// insert to set redis
+	out3 := &D{}
+	cache.Select(context.TODO(), "key1").Upsert(10, &D{A: "setter done", B: 10}, setDataRedis)
+	cache.Select(context.TODO(), "key1").Get(10, nil).Exec(out3)
+	if out3.B != 10 {
+		panic("fail")
+	}
 }
